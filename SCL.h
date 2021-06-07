382270bbd3483eab167e811058a8101abee6e940
MartianCheats <85505898+MartianCheats@users.noreply.github.com>
2021-06-07 17:41:36 +0200

    Add files via upload

---
+++ SCL.h
@@ -0,0 +1,66 @@
+#pragma once
+#include<iostream>
+#include<Windows.h>
+
+using namespace std;
+
+HWND hwnd;
+DWORD procID;
+HANDLE handle;
+
+std::string print(std::string text)
+{
+	std::cout << text << std::endl;
+	return text;
+}
+
+const char* read()
+{
+	std::string msg;
+	std::cin >> msg;
+	return 0;
+}
+
+void StartProcess(const char* pPath)
+{
+	system(pPath);
+}
+
+bool AttachToProcess(const char* wName)
+{
+	hwnd = FindWindowA(NULL, wName); 
+	GetWindowThreadProcessId(hwnd, &procID);
+	handle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, procID);
+	return true;
+}
+
+void WriteMemory(int memAdress, int newValue)
+{
+	WriteProcessMemory(handle, (LPVOID)memAdress, &newValue, sizeof(newValue), 0);
+}
+
+void ReadMemory(int memAdress)
+{
+	ReadProcessMemory(handle, (LPVOID)memAdress, &memAdress, sizeof(memAdress), 0);
+}
+
+void Detach()
+{
+	Sleep(2000);
+	exit(-1);
+}
+
+void AC_PROOF_MODE(std::string mode)
+{
+	DWORD vacID;
+	HWND vacProc = FindWindowA(NULL, "VAC.exe");
+	if(mode == "vac")
+	{
+		HANDLE vacHandle = OpenProcess(PROCESS_ALL_ACCESS, FALSE, vacProc);
+		GetWindowThreadProcessId(vacProc, &vacID);
+	}
+	else
+	{
+		std::cout << "Bypass not Supported yet" << std::endl;
+	}
+}
